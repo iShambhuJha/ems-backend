a691df7dbec69a1769c783a7424b661c6a758f83
shambhu jha <shambhu.jha014@gmail.com>
2022-07-25 14:50:56 +0530

    MySql db connection setup done

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const mySql = require("mysql");
+
+var mySqlConnection  = mySql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database: 'empdb'
+});
+
+mySqlConnection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 const app = express();
 app.listen(3000,() => console.log("Server listening at port 3000"));
 
